f1d95372272bbe0c14cac6f4c149e99516eccc73
Adamas <adamas@tom.com>
2016-04-17 23:35:29 +0800

    Done

--- crawler/zgallerie/zgallerie.py
+++ crawler/zgallerie/zgallerie.py
@@ -46,77 +46,13 @@ class ZgallerieSpider(scrapy.Spider):
     def parse_item(self, response):
         pic_id = 'ProductPic' + response.url.split('/')[-1].split('-')[1]
         image_url = response.xpath('//*[@id="' + pic_id + '"]/@src').extract()[0]
-
         image_name = image_url.split('/')[-1]
+
+        HiddenProductDetails = response.xpath('//*[@id="HiddenProductDetails"]').extract()[0]
+        r = re.compile('.*SjZViewer\((.*)\)')
+        m = r.match(HiddenProductDetails.replace('\'', '')).groups()[0]
+        url = "".join(m.split(',')[0:2]) + "?rect=0,0,2000,2000&scl=2.315789473684211"
+
         img_path = images_path + image_name
-        urllib.urlretrieve(image_url, img_path)
+        urllib.urlretrieve(url, img_path)
         self.logger.info("Got " + image_url)
-
-        '''
-response.xpath('//*[@id="HiddenProductDetails"]').extract()[0]
-
-like:
-<div id="HiddenProductDetails" border="0" width="100%" cellpadding="0" cellspacing="0" style="zoom: 1;visibility:hidden;"><div
-id="galleryPopup" style="width: 0px; height:0px;border:1px solid #cccccc; cursor: pointer; position:
-relative;padding-bottom:12px"><div id="apDiv5"><div id="izNav" style="position: relative; width: 100px; height:
-100px;"></div></div><div id="izView" style="width: 817px; height: 475px; cursor: move; position: absolute;  left: 116px;
-border-left:1px solid #cccccc;top:2px;text-align: center; z-index: 3;"> </div><script language="JavaScript"
-type="text/javascript">var s7zoom=new SjZViewer(\'http://images.zgallerie.com/is/image/ZGallerie/\',\'730250472\', 817, 475,
-\'3gEKcutzs1XImKMFnYJWly\');var s7navigator= new SjZoomNav(null, null, null, "relative");var zoomsteps = 2;var zoomlimit =
-100;s7zoom.setBackground("0xffffff");s7zoom.initialRGNA("500,500,1100,1100");s7zoom.setBorder("0",
-"#ccc");s7zoom.setFadeTime(.3);s7zoom.setMaxZoom(zoomlimit);s7zoom.setZoomStep(zoomsteps);s7zoom.setFormat("jpeg");if(S7ConfigClient.isVersion
-== "2.7")s7zoom.setCachingModel("");s7zoom.addInformation("To Zoom click on the image.\\\\nTo Pan, click and hold the mouse while
-dragging.");s7zoom.setHelpPage("www.scene7.com", "100,100");s7zoom.addToPage();s7navigator = new SjZoomNav(null, null, null,
-"relative");s7navigator.setViewer(s7zoom.zviewer);s7navigator.visible(true);s7zoom.enableNav(0, 10, 466, 100,
-100);s7zoom.setBorderNav(5, "#CCCCCC"); </script><div id="apDiv1"><div id="close1" onclick="galleryPopupClose()"></div></div><div
-id="apDiv2"><div class="galleryThums"><div class="zoomPic"><table id="idzoomPictable" class="zoomPictable" cellpadding="0"
-cellspacing="0" margin="0"> <tr><td><img src="images/zoom_01.jpg" width="20" height="22" valign="top" align="left"></td><td><a
-href="javascript:s7zoom.zoomOut()"><img src="images/zoom_minus.jpg" name="btn_zoomout" valign="top" align="left" width="16"
-height="22" onmouseover="chgImage(\'btn_zoomout\',\'zoomoutImg_on\')"
-onmouseout="chgImage(\'btn_zoomout\',\'zoomoutImg_off\')"></a></td><td><img src="images/zoom_03.jpg" width="4" height="22"
-align="left" valign="top"></td><td><a href="javascript:s7zoom.zoomIn();"><img src="images/zoom_plus.jpg" valign="top"
-name="btn_zoomin" width="16" height="22" align="left" onmouseover="chgImage(\'btn_zoomin\',\'zoominImg_on\')"
-onmouseout="chgImage(\'btn_zoomin\',\'zoominImg_off\')"></a></td><td><a href="javascript:s7zoom.zoomIn();"><img
-src="images/zoom_05.jpg" width="48" height="22" align="left" valign="top"></a></td></tr></table></div><div
-class="galleryThums2"><ul id="mycarousel" class="jcarousel-skin-tango"><li style="width:auto;border:none;"><a
-href="javascript:changeImage(\'730250472\',\'http://images.zgallerie.com/is/image/ZGallerie\')"><img
-src="http://images.zgallerie.com/is/image/ZGallerie/zoomthumb/space-between-730250472.jpg" width="68" height="68"
-alt=""></a></li><li style="width:auto;border:none;"><a
-href="javascript:changeImage(\'730250472_1\',\'http://images.zgallerie.com/is/image/ZGallerie\')"><img
-src="http://images.zgallerie.com/is/image/ZGallerie/zoomthumb/space-between-730250472_1.jpg" width="68" height="68"
-alt=""></a></li><li style="width:auto;border:none;"><a
-href="javascript:changeImage(\'730250472_2\',\'http://images.zgallerie.com/is/image/ZGallerie\')"><img
-src="http://images.zgallerie.com/is/image/ZGallerie/zoomthumb/space-between-730250472_2.jpg" width="68" height="68"
-alt=""></a></li></ul></div></div></div></div></div>
-
-get:
-    var s7zoom=new SjZViewer(\'http://images.zgallerie.com/is/image/ZGallerie/\',\'730250472\', 817, 475,
-    \'3gEKcutzs1XImKMFnYJWly\');
-
-and the url is
-    http://images.zgallerie.com/is/image/ZGallerie/730250472?rect=0,166,907,663&scl=2.315789473684211
-
-All script:
-    <script language="JavaScript" type="text/javascript">
-    var s7zoom=new SjZViewer(\'http://images.zgallerie.com/is/image/ZGallerie/\',\'730250472\', 817, 475, \'3gEKcutzs1XImKMFnYJWly\');
-    var s7navigator= new SjZoomNav(null, null, null, "relative");
-    var zoomsteps = 2;
-    var zoomlimit = 100;
-    s7zoom.setBackground("0xffffff");
-    s7zoom.initialRGNA("500,500,1100,1100");
-    s7zoom.setBorder("0", "#ccc");
-    s7zoom.setFadeTime(.3);
-    s7zoom.setMaxZoom(zoomlimit);
-    s7zoom.setZoomStep(zoomsteps);
-    s7zoom.setFormat("jpeg");
-    if(S7ConfigClient.isVersion == "2.7")s7zoom.setCachingModel("");
-    s7zoom.addInformation("To Zoom click on the image.\\\\nTo Pan, click and hold the mouse while dragging.");
-    s7zoom.setHelpPage("www.scene7.com", "100,100");
-    s7zoom.addToPage();
-    s7navigator = new SjZoomNav(null, null, null, "relative");
-    s7navigator.setViewer(s7zoom.zviewer);
-    s7navigator.visible(true);
-    s7zoom.enableNav(0, 10, 466, 100, 100);
-    s7zoom.setBorderNav(5, "#CCCCCC");
-     </script>
-        '''
