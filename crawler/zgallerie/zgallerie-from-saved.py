9a831e519bd07df5844a4100768ebea1e4a14cfd
Adamas <adamas@tom.com>
2016-04-19 10:06:58 +0800

    Crawl from a local file, which is saved from the original website.
    The network here is slow, so I asked her to save the file, and send it
    to me, so I can get the concrete URLs from the saved file and reach them
    directly.

--- crawler/zgallerie/zgallerie-from-saved.py
+++ crawler/zgallerie/zgallerie-from-saved.py
@@ -11,7 +11,9 @@ import time
 # TODO(adamas): move it to a configuration file
 images_path="images/"
 
-root_url = 'http://www.zgallerie.com'
+cwd_path=os.getcwd()
+saved_html_name="Stylish.html"
+file_url = "file://" + cwd_path + "/" + saved_html_name
 
 class ZgallerieSpider(scrapy.Spider):
 
@@ -24,8 +26,7 @@ class ZgallerieSpider(scrapy.Spider):
         url_done = []
 
     start_urls = [
-            #"http://www.zgallerie.com/ViewAll.aspx?N=2000040",
-            "file:///home/adamas/tmp/adamas-tools/crawler/zgallerie/Stylish.html"
+            file_url
                  ]
 
     def parse(self, response):
@@ -33,29 +34,24 @@ class ZgallerieSpider(scrapy.Spider):
 
         for image in image_list[500:]:
             img_detail_url = image.xpath('@href').extract()[0]
-            image_url = "http:" + image.xpath('img/@src').extract()[0]
-
-            img_path = images_path + img_detail_url.split('.')[0] + "-s.jpg"
-            #urllib.urlretrieve(image_url, img_path)
-            #self.logger.info("Got " + image_url)
-
-            dest_url = root_url + img_detail_url
-            #pic_id = 'ProductPic' + img_detail_url.split('.')[0].split('-')[1]
 
             self.logger.info("Go to " + img_detail_url)
             time.sleep(10)
             yield Request(img_detail_url.replace('https', 'http'), callback=self.parse_item)
 
     def parse_item(self, response):
+        # get the image name
         pic_id = 'ProductPic' + response.url.split('/')[-1].split('-')[1]
         image_url = response.xpath('//*[@id="' + pic_id + '"]/@src').extract()[0]
         image_name = image_url.split('/')[-1]
 
+        # get the image url
         HiddenProductDetails = response.xpath('//*[@id="HiddenProductDetails"]').extract()[0]
         r = re.compile('.*SjZViewer\((.*)\)')
         m = r.match(HiddenProductDetails.replace('\'', '')).groups()[0]
         url = "".join(m.split(',')[0:2]) + "?rect=0,0,1000,1000&scl=2.315789473684211"
 
+        # download
         img_path = images_path + image_name
         if not os.access(img_path, os.R_OK):
             urllib.urlretrieve(url, img_path)
